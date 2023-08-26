b7419b97e30709694574691ffeb7b169ad145049
b.kulish <vremapriklucenij16@gmail.com>
2023-08-27 00:27:28 +0300

    fix html template

--- WebApi/app.py
+++ WebApi/app.py
@@ -3,9 +3,5 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('lda.html', title='Home')
-
-if __name__ == '__main__':
-	app.run(debug=True)
